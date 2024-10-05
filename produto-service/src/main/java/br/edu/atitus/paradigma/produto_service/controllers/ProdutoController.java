219411f5d270767b904a92d7e525ad2d1992ba85
Pedro Henrique De Bortoli <lopesdebortoli@gmail.com>
2024-10-05 19:34:42 -0300

    recomeço do projeto

---
+++ produto-service/src/main/java/br/edu/atitus/paradigma/produto_service/controllers/ProdutoController.java
@@ -0,0 +1,48 @@
+package br.edu.atitus.paradigma.produto_service.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.paradigma.produto_service.entities.ProdutoEntity;
+import br.edu.atitus.paradigma.produto_service.repositories.ProdutoRepository;
+
+@RestController
+@RequestMapping("produto-service")
+public class ProdutoController {
+	
+	private final ProdutoRepository produtoRepository;
+		
+	public ProdutoController(ProdutoRepository produtoRepository) {
+		super();
+		this.produtoRepository = produtoRepository;
+	}
+
+	@Value("${server.port}")
+	private int porta;
+	
+	@GetMapping("/{idProduto}/{moeda}")
+	public ResponseEntity<ProdutoEntity> getProduto(
+			@PathVariable Integer idProduto,
+			@PathVariable String moeda) throws Exception{
+		
+		ProdutoEntity produto = produtoRepository.findById(idProduto).orElseThrow(() -> new Exception("Produto não encontrado!"));
+		
+		//Setar o ambiente
+		produto.setAmbiente("Produto-Service run in port: " + porta);
+		return ResponseEntity.ok(produto);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception e) {
+		String cleanMessage = e.getMessage().replaceAll("[\\r\\n]", " ");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(cleanMessage);
+	}
+
+}
