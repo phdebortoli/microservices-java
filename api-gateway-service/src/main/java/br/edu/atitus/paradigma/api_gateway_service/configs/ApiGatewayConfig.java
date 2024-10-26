c720aacbcdd45f7d68cc8067ccf186e49f6dc3aa
Pedro Henrique De Bortoli <lopesdebortoli@gmail.com>
2024-10-25 21:23:28 -0300

    API Gateway

---
+++ api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -0,0 +1,32 @@
+package br.edu.atitus.paradigma.api_gateway_service.configs;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+	
+	@Bean
+	RouteLocator getRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(rota -> rota
+						.path("/get")
+						.filters(f -> f
+								.addRequestHeader("Usuario: ", "Pedro Henrique De Bortoli")
+								.addResponseHeader("Teste", "ResponseHeader"))
+						.uri("http://httpbin.org"))
+				.route(rota -> rota
+						.path("/cambio-service/**")
+						.uri("lb://cambio-service"))
+				.route(rota -> rota
+						.path("/produto-service/**")
+						.uri("lb://produto-service"))
+				.route(rota -> rota
+						.path("/saudacao-service/**")
+						.uri("lb://saudacao-service"))
+				.build();
+	}
+
+}
